81cb7a95f434c10351d933852e94f06e86f551da
vivek201102 <vivekpatel201102@gmail.com>
2022-08-06 15:19:58 +0530

    project_init

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: http://localhost:${port}`);
+});
