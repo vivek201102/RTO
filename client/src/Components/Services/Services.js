e707d50c96439f9d7b16ef2d44720f8f3b39137b
Vivek Patel <69710864+vivek201102@users.noreply.github.com>
2022-11-04 21:50:20 +0530

    old services

--- client/src/Components/Services/Services.js
+++ client/src/Components/Services/Services.js
@@ -1,62 +1,58 @@
 import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle"
+// import { Dropdown } from "bootstrap-4-react/lib/components"
 import './Services.css'
 import { Link } from "react-router-dom"
-import Dropdown from 'react-dropdown';
+// import Dropdown from 'react-dropdown';
 
 export default function Services() {
-//     const [age, setAge] = React.useState('');
+    //     const [age, setAge] = React.useState('');
 
-//   const handleChange = (event: Selec) => {
-//     setAge(event.target.value as string);
-  
+    //   const handleChange = (event: Selec) => {
+    //     setAge(event.target.value as string);
+
     return (
         <>
             <div className="container whole">
                 <div className="head container">Services we provide...</div><br />
-                <div class="row">
-                    <div class="col-sm-3 width">
-                        <div class="card">
-                            <div class="card-body">
+                <div className="row">
+                    <div className="col-sm-3 width">
+                        <div className="card">
+                            <div className="card-body">
                                 <img src="https://parivahan.gov.in/parivahan//sites/default/files/images/v-learners-license-services.png" alt="Driving School" />
-                                <p class="card-text"><h5>Driving/Learning License:</h5></p>
-                                <p class="card-text">Apply for License</p>
-                                <center><Link to="/licence" class="btn btn-primary">Apply Now</Link></center>
+                                <h5><p className="card-text">Driving/Learning License:</p></h5>
+                                <p className="card-text">Apply for License</p>
+                                <center>
+                                <div className="dropdown">
+                                    <button className="btn btn-secondary dropdown-toggle bg-primary"  type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Apply Now   
+                                    </button>
+                                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <Link className="dropdown-item" to="/licence">Apply for Learning license</Link>
+                                        <Link className="dropdown-item" to="/licence">Apply for Driving licence</Link>
+                                    </div>
+                                </div>
+                                </center>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-3 width">
-                        <div class="card">
-                            <div class="card-body">
+                    <div className="col-sm-3 width">
+                        <div className="card">
+                            <div className="card-body">
                                 <img src="https://parivahan.gov.in/parivahan//sites/default/files/images/v-driving-school-license.png" alt="Driving School" />
-                                <p class="card-text"><h5>Driving School:</h5></p>
-                                <p class="card-text">Registering for Driving School</p>
-                                <center><Link to="/drivingschool" class="btn btn-primary">Apply Now</Link></center>
-                                {/* <Box sx={{ minWidth: 120 }}>
-                                    <FormControl fullWidth>
-                                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                                        <Select
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            value={age}
-                                            label="Age"
-                                            onChange={handleChange}
-                                        >
-                                            <MenuItem value={10}>Ten</MenuItem>
-                                            <MenuItem value={20}>Twenty</MenuItem>
-                                            <MenuItem value={30}>Thirty</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                </Box> */}
+                                <h5><p className="card-text">Driving School:</p></h5>
+                                <p className="card-text">Registering for Driving School</p>
+                                <center><Link to="/drivingschool" className="btn btn-primary">Apply Now</Link></center>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-3 width">
-                        <div class="card">
-                            <div class="card-body">
+                    <div className="col-sm-3 width">
+                        <div className="card">
+                            <div className="card-body">
                                 <img src="https://parivahan.gov.in/parivahan//sites/default/files/images/v-fancy-number-allocation.png" alt="Driving School" />
-                                <p class="card-text"><h5>Special Number:</h5></p>
-                                <p class="card-text">Apply for Special No. of your Vehicle</p>
-                                <center><Link to="/specialnumber" class="btn btn-primary">Apply Now</Link></center>
+                                <h5><p className="card-text">Special Number:</p></h5>
+                                <p className="card-text">Apply for Special No. of your Vehicle</p>
+                                <center><Link to="/specialnumber" className="btn btn-primary">Apply Now</Link></center>
                             </div>
                         </div>
                     </div>
@@ -66,15 +62,3 @@ export default function Services() {
     )
 }
 
-//demo
-
-{/* <div class="service-sec center-position">
-                        <p class="center-position"> 
-                        <a href="https://sarathi.parivahan.gov.in/sarathiservice/" target="_blank">
-                        <img src="https://parivahan.gov.in/parivahan//sites/default/files/images/v-driving-school-license.png" alt="Driving School"> 
-                        </a>
-                        </p>
-                        <h5 class="center-position">Driving School</h5>
-                        <p class="left-position">One place for application of Driving School License</p>    
-                        <a href="https://sarathi.parivahan.gov.in/sarathiservice/" target="_blank" class="bttn-more">More</a>
- </div> */}
