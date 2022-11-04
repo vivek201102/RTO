fa3fff17c35cbaed94f1a8e20bb11b21e79f5ef3
jayshah2002 <jaykshah2002@gmail.com>
2022-11-04 21:35:40 +0530

    updated services

--- client/src/Components/SpecialNumber.js
+++ client/src/Components/SpecialNumber.js
@@ -2,7 +2,15 @@ export default function Specialnumber(){
 
     return (
         <>
-       Specialnumber
+        <section className="specialno">
+                
+                <form className="form">
+                 
+                    <input type="text" placeholder='Enter your Username'required/>
+                    <input type="text" placeholder='Enter your Special number' required/>
+                    <input type="submit"   value="Apply for Special Number" style={{ backgroundColor: "#a1eafb" }} />
+                </form>
+                </section>
         </>
     )
 }
